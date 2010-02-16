5819b3f0c89dde6509720965196a646c95f2171f
johnbent <johnbent@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2010-02-16 16:38:21 +0000

    added a TODO comment in f_release about setting correct supplementary groups
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/branches/library@82 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- fuse/plfs_fuse.cpp
+++ fuse/plfs_fuse.cpp
@@ -798,6 +798,7 @@ int Plfs::f_release( const char *path, struct fuse_file_info *fi ) {
     // there is one 'Plfs_fd *of' shared by multiple procs
     // each proc has its own 'OpenFile openfile'
     if ( of ) {
+        // TODO, we also need to set correct groups here
         Util::Setfsuid( openfile->uid );
         Util::Setfsgid( openfile->gid );
         Util::MutexLock( &self->fd_mutex, __FUNCTION__ );
